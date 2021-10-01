ef60cf08d227d67de9dea46c615d96fffdd5f2ff
井上　太治朗 <87561665+TaijiroInoue@users.noreply.github.com>
2021-10-01 11:18:02 +0900

    Add files via upload

---
+++ kadai08/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+function connect_to_db()
+{
+    $dbn='mysql:dbname=gsacf_d03_02;charset=utf8;port=3306;host=localhost';
+    $user = 'root';
+    $pwd = '';
+    
+    try {
+    return new PDO($dbn, $user, $pwd);
+    } catch (PDOException $e) {
+    echo json_encode(["db error" => "{$e->getMassage()}"]);
+    exit();
+    }
+}
+
+?>
